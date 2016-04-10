4c73d7543fb58874abaadc59d95544b1a4936164
annafractuous <anna.nigma@yahoo.com>
2016-04-10 14:22:44 -0400

    Add profile percent complete function that is abstractable to new category tables and questions/columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,4 +34,43 @@ class User < ActiveRecord::Base
  validates_uniqueness_of :email, :username
  validates_presence_of :password, on: :create
  validates_confirmation_of :password
+
+ def profile_percent_complete
+   total_questions = 0
+   questions_answered = 0
+
+   User.user_columns.each do |col|
+     total_questions += 1
+     questions_answered += 1 if self.send(col) != nil
+   end
+
+   User.question_tables.each do |category|
+     if self.send(category.singularize)
+       User.question_columns(category).each do |col|
+         total_questions += 1
+         questions_answered += 1 if self.send(category.singularize).send(col) != nil
+       end
+     else
+       total_questions += User.question_columns(category).size
+     end
+   end
+
+   ((questions_answered/total_questions.to_f) * 100).to_i
+ end
+
+ private
+
+   def self.question_tables
+     ActiveRecord::Base.connection.tables.reject { |t| ["users", "match_connections", "schema_migrations"].include?(t) }
+   end
+
+   def self.user_columns
+     self.column_names.reject { |col| ["id", "name", "username", "email", "password_digest", "created_at", "updated_at"].include?(col) }
+   end
+
+   def self.question_columns(table)
+     table = Object.const_get(table.classify)
+     table.column_names.reject { |col| ["id", "user_id", "created_at", "updated_at"].include?(col) }
+   end
+
 end
