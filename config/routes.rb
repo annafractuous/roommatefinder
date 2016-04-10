4cd3d4dcadd82b547341cb6e4493cf735a4d3274
annafractuous <anna.nigma@yahoo.com>
2016-04-10 16:14:29 -0400

    Add bootstrap navbar, rename home path to root_path

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new', as: 'login'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy', as: 'logout'
-  get '/', to: 'pages#home', as: 'home'
+  get '/', to: 'pages#home', as: 'root'
   resources :users do
     resources :cleanlinesses
     resources :habits
