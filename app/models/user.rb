3be006981a20310913cc999bc06f912569bdf6c2
alexgriff <alexalexgriffith@gmail.com>
2016-05-04 16:59:09 -0400

    caching changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-
   has_secure_password
-
+  
   has_attached_file :photo, styles: { small: "200x200"}
   validates_attachment_content_type :photo, content_type: /\Aimage/
   validates_attachment_file_name :photo, matches: [/png\Z/, /jpe?g\Z/]
